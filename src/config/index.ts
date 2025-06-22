265fcce57570fce6573650da23e23a00c98a2b5b
Aily-MStanek <mstanekofficial@gmail.com>
2025-06-22 16:03:46 +0200

    Vibe coding is soo cool

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-const title = 'React PWA';
+const title = 'CS2 Sound Guesser';
 
-const email = 'auther-email@gmail.com';
+const email = 'contact@cs2soundguesser.com';
 
-const repository = 'https://github.com/suren-atoyan/react-pwa';
+const repository = 'https://github.com/mstan/cs2guesser';
 
 const dateFormat = 'MMMM DD, YYYY';
 
@@ -13,8 +13,8 @@ const loader = {
 };
 
 const defaultMetaTags = {
-  image: '/cover.png',
-  description: 'Starter kit for modern web applications',
+  image: '/cs2logo.jpg',
+  description: 'CS2 Sound Guesser - Test your knowledge of Counter-Strike 2 weapon sounds! Listen to weapon sounds and guess which weapon it is.',
 };
 
 export { loader, dateFormat, repository, email, title, defaultMetaTags };
